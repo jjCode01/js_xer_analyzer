03eadb0ab3df9444369dbf7d2fddfe17145174a8
jjCode01 <code@seqmanagement.com>
2021-11-13 21:14:23 -0800

    refactor taskVariance

--- static/js/taskVariance.js
+++ static/js/taskVariance.js
@@ -38,15 +38,33 @@ function findLogicChanges(proj1, proj2) {
 function findTaskChanges(proj1, proj2){
     const changes = {
         addedTasks: new Change(
-            desc="Added Activities",
-            labels=["Task ID", "Task Name"]
+            "Added Activities",
+            ["Task ID", "Task Name"]
         ),
-        deletedTasks: new Change("Deleted Activities", ["Task ID", "Task Name"]),
-        names: new Change("Activity Name Changes", ["Task ID", "New Task Name", "Old Task Name"]),
-        durations: new Change("Original Duration Changes", ["Task ID", "Task Name", "New Dur", "Old Dur", "Var"]),
-        calendars: new Change("Calendar Assignment Changes", ["Task ID", "Task Name", "New Cal", "Old Cal"]),
-        actualStarts: new Change("Actual Start Date Changes", ["Task ID", "Task Name", "New Start", "Old Start"]),
-        actualFinishes: new Change("Actual Finish Date Changes", ["Task ID", "Task Name", "New Finish", "Old Finish"])
+        deletedTasks: new Change(
+            "Deleted Activities",
+            ["Task ID", "Task Name"]
+        ),
+        names: new Change(
+            "Activity Name Changes",
+            ["Task ID", "New Task Name", "Old Task Name"]
+        ),
+        durations: new Change(
+            "Original Duration Changes",
+            ["Task ID", "Task Name", "New Dur", "Old Dur", "Var"]
+        ),
+        calendars: new Change(
+            "Calendar Assignment Changes",
+            ["Task ID", "Task Name", "New Cal", "Old Cal"]
+        ),
+        actualStarts: new Change(
+            "Actual Start Date Changes",
+            ["Task ID", "Task Name", "New Start", "Old Start"]
+        ),
+        actualFinishes: new Change(
+            "Actual Finish Date Changes",
+            ["Task ID", "Task Name", "New Finish", "Old Finish"]
+        )
     }
 
     const currTasks = new Map(proj1.tasks.map(t => [t.task_code, t])) 
@@ -55,39 +73,68 @@ function findTaskChanges(proj1, proj2){
     // Find task changes
     currTasks.forEach((task, key) => {
         // Find added tasks
-        if (!prevTasks.has(key)){changes.addedTasks.add([task.task_code, task.task_name])}
+        if (!prevTasks.has(key)){
+            changes.addedTasks.add([
+                task.task_code,
+                task.task_name
+            ])
+        }
         else{
             const prev = prevTasks.get(key)
-            console.log(prev)
 
             // Find Name Change
             if (task.task_name != prev.task_name){
-                changes.names.add([task.task_code, task.task_name, prev.task_name])
+                changes.names.add([
+                    task.task_code,
+                    task.task_name,
+                    prev.task_name
+                ])
             }
 
             // Find orig duration change
             if (task.target_drtn_hr_cnt != prev.target_drtn_hr_cnt){
                 const newDur = task.target_drtn_hr_cnt / 8
                 const oldDur = prev.target_drtn_hr_cnt / 8
-                changes.durations.add([task.task_code, task.task_name, newDur, oldDur, newDur - oldDur])
+                changes.durations.add([
+                    task.task_code,
+                    task.task_name,
+                    newDur,
+                    oldDur,
+                    newDur - oldDur
+                ])
             }
 
             // Find Calendar assignment changes
             if (task.calendar.clndr_name != prev.calendar.clndr_name){
-                changes.calendars.add([task.task_code, task.task_name, task.calendar.clndr_name, prev.calendar.clndr_name])
+                changes.calendars.add([
+                    task.task_code,
+                    task.task_name,
+                    task.calendar.clndr_name,
+                    prev.calendar.clndr_name
+                ])
             }
 
             // Find Actual Start changes
             if (!task.notStarted && !prev.notStarted) {
                 if (formatDate(task.start) != formatDate(prev.start)) {
-                    changes.actualStarts.add([task.task_code, task.task_name, formatDate(task.start), formatDate(prev.start)])
+                    changes.actualStarts.add([
+                        task.task_code,
+                        task.task_name,
+                        formatDate(task.start),
+                        formatDate(prev.start)
+                    ])
                 }
             }
 
-            // // Find Actual Finish changes
+            // Find Actual Finish changes
             if (task.completed && prev.completed) {
                 if (formatDate(task.finish) != formatDate(prev.finish)) {
-                    changes.actualFinishes.add([task.task_code, task.task_name, formatDate(task.finish), formatDate(prev.finish)])
+                    changes.actualFinishes.add([
+                        task.task_code,
+                        task.task_name,
+                        formatDate(task.finish),
+                        formatDate(prev.finish)
+                    ])
                 }
             }
         }
@@ -95,7 +142,10 @@ function findTaskChanges(proj1, proj2){
 
     Object.entries(prevTasks).forEach(([key, task]) => {
         if (!currTasks[key]){
-            changes.deletedTasks.add([task.task_code, task.task_name])
+            changes.deletedTasks.add([
+                task.task_code,
+                task.task_name
+            ])
         }
     })
 
